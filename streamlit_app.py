cb6b612d1bdfab1732f53bf9d5fffb7e3a67530e
varalakshmi2 <109960739+varalakshmi2@users.noreply.github.com>
2022-07-25 17:33:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ streamlit.header('Breakfast Names')
 streamlit.text('dosa')
 streamlit.text('puri')
 streamlit.text('bonda')
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
