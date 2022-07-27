79034490894e62f93230393c614808d989fda086
varalakshmi2 <109960739+varalakshmi2@users.noreply.github.com>
2022-07-27 18:40:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+
 
 
 
